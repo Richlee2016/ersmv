225eaadd3499e2c9b00540550dd4cd046e6b514f
lifu <lifu@tuzhanai.com>
2019-06-18 17:34:02 +0800

    添加 less  以及 静态目录内容

--- app/web/page/home/container.tsx
+++ app/web/page/home/container.tsx
@@ -1,6 +1,19 @@
 import React from 'react';
 import { HomeProps } from '../../framework/type';
-
+import './home.less';
 export default function Container(props: HomeProps) {
-  return <div>321</div>;
+  return (
+    <div>
+      <div>
+        <p>321</p>
+        <ul className="box">
+          <li>1</li>
+          <li>321</li>
+          <li>55</li>
+        </ul>
+        <div className="nice" />
+        <img src="/public/images/loading.gif" alt="" />
+      </div>
+    </div>
+  );
 }
